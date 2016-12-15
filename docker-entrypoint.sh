ad25d4897237e8864deb936d8e366e6a296efa17
Ian Droogmans <ian@medianeut.be>
2016-12-15 19:59:52 +0100

    Add correct rights, install newer version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 USER_ID=${LOCAL_USER_ID:-0}
 USERNAME=${LOCAL_USERNAME:-user}
 CHOWNDIRS=${CHOWNDIRS}
+HTML_ROOT=/var/www/html
 
 # exit when a subcommand or pipeline returns a non-zero status
 set -e
@@ -16,33 +17,7 @@ if ! [ -e user/config/system.yaml ]; then
   rsync -a /tmp/grav-$GRAV_VERSION/user/ /var/www/html/user
 fi
 
-
-#make sure user rights are correct
-case ${USER_ID} in
-   "0")
-        # Run as root
-        exec "$@"
-        ;;
-   *)
-        # Run as non-root
-        adduser -s /bin/bash -u ${USER_ID} -D -h /home/${USERNAME} ${USERNAME}
-        export HOME=/home/${USERNAME}
-
-        # chown dirsp
-        if [ "${CHOWNDIRS}" ];
-        then
-            IFS=',' read -r -a array <<< "${CHOWNDIRS}"
-            for element in "${array[@]}"
-            do
-                printf "transfering directory ownership for ${element} to ${USER_ID}:${USER_ID} ...\n"
-                chown -R ${USER_ID}:${USER_ID} ${element};
-            done
-        fi
-
-        # Exec statement
-        exec su-exec ${USERNAME} "$@"
-        ;;
-esac
+chown -R www-data:www-data HTML_ROOT;
 
 echo >&2 "Complete! Grav has been successfully installed"
 
