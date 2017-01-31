08790693fbe92e8dbeb37120e6c37be90ed9172b
Ian Droogmans <ian@medianeut.be>
2017-01-31 21:29:52 +0100

    bump version to 1.1.15, fix permission issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,20 +5,23 @@ USERNAME=${LOCAL_USERNAME:-user}
 CHOWNDIRS=${CHOWNDIRS}
 HTML_ROOT=/var/www/html
 
-# exit when a subcommand or pipeline returns a non-zero status
 set -e
-
-# enable extended patern matching
 shopt -s extglob
 
-# if user directory is not yet synced -> rsync
 if ! [ -e user/config/system.yaml ]; then
   echo >&2 "No userfiles found - copying now..."
   rsync -a /tmp/grav-$GRAV_VERSION/user/ /var/www/html/user
 fi
 
-chown -R www-data:www-data ${HTML_ROOT};
+echo >&2 "Setting permissions to www-data ${HTML_ROOT}"
+chown -R www-data:www-data ${HTML_ROOT}
+find ${HTML_ROOT} -type f -print0 | xargs -0 chmod 664
+find ${HTML_ROOT}/bin -type f -print0 | xargs -0 chmod 775
+find ${HTML_ROOT} -type d -print0 | xargs -0 chmod 775
+find ${HTML_ROOT} -type d -print0 | xargs -0 chmod +s
+umask 0002
 
+bin/gpm install login form email admin
 echo >&2 "Complete! Grav has been successfully installed"
 
 exec "$@"
